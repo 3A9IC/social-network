b8c009509e60bc92b8c947dd191bbe470b438818
Vlad Tsarkov <Zaykoman@gmail.com>
2017-05-26 04:52:55 +0500

    Add files via upload

--- filmsocial/loginsys/urls.py
+++ filmsocial/loginsys/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url,include
 from loginsys import views
 
 urlpatterns = [
-	#url(r'^login/','loginsys.views.login'),
-	#url(r'^logout/','loginsys.views.logout'),
 	url(r'^login/', views.login, name='login'),
 	url(r'^logout/',views.logout, name='logout'),
+	url(r'^register/',views.register, name='register'),
 ]
--- filmsocial/loginsys/views.py
+++ filmsocial/loginsys/views.py
@@ -2,29 +2,36 @@ from django.shortcuts import render,render_to_response,redirect
 from blog.models import base_menu 
 from django.contrib import auth
 from django.template.context_processors import csrf
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login(request):
 	args = {}
 	args.update(csrf(request))
-	#args.update(request)
-	if request.POST:
-		username = request.POST.get('username', '')
-		password = request.POST.get('password', '')
-		user = auth.authenticate(username=username, password=password)
-		if user is not None:
-			auth.login(request, user)
-			return redirect('/blog')#!!!!
-		else:
-			args['login_error'] = "Пользователь не найден"
-			#return render_to_response('login.html', args)
-			return render(request, 'loginsys/login.html', args )
-			#return render(request, 'loginsys/login.html')
+	username1 = request.POST.get('Username')
+	password1 = request.POST.get('Password', '')
+	user = authenticate(username=username1, password=password1)
+	if user is not None:
+		auth.login(request, user)
+		return redirect('/blog')
 	else:
-		#return render_to_response('login.html', args)
-		return render(request, 'loginsys/login.html', args ) #добавляет строчку ВХОД в меню на ссылку /auth/login/
-		#return render(request, 'loginsys/login.html')
+		args['login_error'] = "Пользователь не найден"
+		return render(request, 'loginsys/login.html', args )
 
 def logout(request):
 	auth.logout(request)
 	return redirect("/blog")
+	
+def register(request):
+	args = {}
+	args.update(csrf(request))
+	args['form'] = UserCreationForm()
+	if request.POST:
+		newuser_form = UserCreationForm(request.POST)
+		if newuser_form.is_valid():
+			newuser_form.save()
+			return redirect('/blog')
+		else:
+			args['form'] = newuser_form
+	return render(request, 'loginsys/register.html', args)
